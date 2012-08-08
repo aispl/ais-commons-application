871feff129b2dd070cac4e8c21d391ce38c2f026
Michal Jastak <warlock@ais.pl>
2012-08-08 07:21:42 +0200

    initial import of Visitor and VisitorAcceptor

---
+++ src/main/java/pl/ais/commons/application/pattern/Visitor.java
@@ -0,0 +1,17 @@
+package pl.ais.commons.application.pattern;
+
+/**
+ * Marker interface for the Visitor.
+ *
+ * @param <V> determines specific type of the Visitor
+ *
+ * @see <a href="http://en.wikipedia.org/wiki/Visitor_pattern">Visitor Pattern</a>
+ *
+ * @author Warlock, AIS.PL
+ * @since 1.0.2
+ */
+public interface Visitor<V extends Visitor<V>> {
+
+    // Empty by design ...
+
+}
