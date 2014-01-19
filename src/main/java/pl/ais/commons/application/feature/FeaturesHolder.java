13bf785517d8f3d32409719f6230c3d5901b30fd
Michał Jaśtak <warlock@ais.pl>
2014-01-19 18:31:21 +0100

    FeaturesHolder mutators dropped

--- src/main/java/pl/ais/commons/application/feature/FeaturesHolder.java
+++ src/main/java/pl/ais/commons/application/feature/FeaturesHolder.java
@@ -11,44 +11,23 @@ import javax.annotation.Nonnull;
 public interface FeaturesHolder {
 
     /**
-     * Adds new feature.
-     *
-     * @param feature the feature to add
-     * @param handler the feature handler
-     */
-    void addFeature(@Nonnull final Class<?> feature, @Nonnull final Object handler);
-
-    /**
-     * Adds new virtual feature.
-     *
-     * @param feature the feature to add
-     */
-    void addVirtualFeature(@Nonnull final Class<?> feature);
-
-    /**
      * Retrieves the feature.
      *
-     * @param feature feature request
-     * @return the requested feature
-     * @throws UnsupportedFeatureException if the requested feature is not owned by this holder
-     * @throws VirtualFeatureException if the requested feature is virtual (has no implementation)
+     * @param <F> determines the type of feature to be retrieved
+     * @param feature feature to be retrieved
+     * @return requested feature
+     * @throws UnsupportedFeatureException if requested feature is not owned by this holder
+     * @throws VirtualFeatureException if requested feature is virtual (has no implementation)
      */
     @Nonnull
-    <T> T getFeature(@Nonnull final Class<T> feature) throws UnsupportedFeatureException, VirtualFeatureException;
+    <F> F getFeature(@Nonnull final Class<F> feature) throws UnsupportedFeatureException, VirtualFeatureException;
 
     /**
      * Verifies if requested feature is owned by the holder.
      *
-     * @param feature feature request
+     * @param feature feature to be verified
      * @return {@code true} if the feature is owned by the holder, {@code false} otherwise
      */
     boolean hasFeature(@Nonnull final Class<?> feature);
-
-    /**
-     * Removes the feature.
-     *
-     * @param feature the feature to remove
-     */
-    void removeFeature(@Nonnull final Class<?> feature);
 
 }
