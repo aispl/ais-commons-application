70f4361cd172bf8c761b2011b648593571cbb573
Michał Jaśtak <warlock@ais.pl>
2014-01-20 11:57:58 +0100

    one more feature added for tests

---
+++ src/test/java/pl/ais/commons/application/feature/internal/FeatureC.java
@@ -0,0 +1,15 @@
+package pl.ais.commons.application.feature.internal;
+
+/**
+ * Example feature.
+ *
+ * @author Warlock, AIS.PL
+ * @since 1.1.1
+ */
+public interface FeatureC {
+
+    /**
+     * Do something usual.
+     */
+    void doSomethingUsual();
+}
