ecb002de7ce8380e913b608ed0ba31d81d60841c
Michał Jaśtak <warlock@ais.pl>
2014-02-11 19:16:39 +0100

    docs updated

--- src/main/java/pl/ais/commons/application/util/jquery/datatables/TabularDataProvider.java
+++ src/main/java/pl/ais/commons/application/util/jquery/datatables/TabularDataProvider.java
@@ -5,16 +5,18 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Defines the API contract for tabular data provider.
+ *
  * @author Warlock, AIS.PL
  * @since 1.1.1
  */
 public interface TabularDataProvider {
 
     /**
-     * Creates a Map holding all informations required by jQuery DataTables for viewing the data.
+     * Creates a Map holding all informations required by jQuery DataTables for viewing tabular data.
      *
      * @param request currently serviced HTTP request
-     * @return a Map holding all informations required by jQuery DataTables for viewing the data
+     * @return a Map holding all informations required by jQuery DataTables for viewing tabular data
      */
     Map<String, Object> inResponseTo(HttpServletRequest request);
 
