2172a8923e09066d844e8a39b411e0a6d7a7cc1c
Michał Jaśtak <warlock@ais.pl>
2014-08-23 16:44:18 +0200

    Visitor and Visitor Acceptor restored for commons-bean compatibility

---
+++ src/main/java/pl/ais/commons/application/pattern/VisitorAcceptor.java
@@ -0,0 +1,23 @@
+package pl.ais.commons.application.pattern;
+
+/**
+ * Defines the API contract for Visitor Acceptor.
+ *
+ * @param <V> determines specific type of the Visitor
+ *
+ * @see <a href="http://en.wikipedia.org/wiki/Visitor_pattern">Visitor Pattern</a>
+ *
+ * @author Warlock, AIS.PL
+ * @since 1.1.1
+ */
+public interface VisitorAcceptor<V extends Visitor<V>> {
+
+    /**
+     * Accepts given visitor.
+     *
+     * @param visitor the visitor to accept
+     * @return value of type {@literal R} specific for concrete visitor implementation
+     */
+    <R> R accept(V visitor);
+
+}
