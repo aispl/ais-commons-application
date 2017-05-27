413dedb76a0c2959197e81cf77c110f3422a760e
Michał Jaśtak <warlock@ais.pl>
2017-05-27 13:37:22 +0200

    MailSender backward incompatible change - it will be i-face from now on, with default implementation.

---
+++ src/main/java/pl/ais/commons/application/notification/mail/DefaultMailSender.java
@@ -0,0 +1,115 @@
+package pl.ais.commons.application.notification.mail;
+
+import pl.ais.commons.application.notification.AddressType;
+import pl.ais.commons.application.notification.AddressedNotification;
+import pl.ais.commons.application.notification.NotificationException;
+import pl.ais.commons.application.notification.component.Subject;
+
+import javax.mail.Address;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.event.TransportListener;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Default {@link MailSender} implementation.
+ *
+ * @author Warlock, AIS.PL
+ * @since 1.2.1
+ */
+public final class DefaultMailSender implements MailSender {
+
+    private final Session session;
+
+    public DefaultMailSender(final Session session) {
+        this.session = session;
+    }
+
+    private static RecipientType mapType(final AddressType type) {
+        final RecipientType result;
+        switch (type) {
+            case BCC:
+                result = RecipientType.BCC;
+                break;
+
+            case CC:
+                result = RecipientType.CC;
+                break;
+
+            default:
+                result = RecipientType.TO;
+                break;
+        }
+        return result;
+    }
+
+    @Override
+    public void accept(final AddressedNotification notification, final TransportListener... listeners) {
+
+        final MimeMessage message = new MimeMessage(session);
+        try {
+            message.setSentDate(new Date());
+
+            final String sender = notification.getSender();
+            if ((null == sender) || sender.isEmpty()) {
+                message.setFrom();
+            } else {
+                final InternetAddress senderAddress = new InternetAddress(sender);
+                message.setFrom(senderAddress);
+                message.setReplyTo(new Address[] {senderAddress});
+            }
+
+            EnumSet.allOf(AddressType.class)
+                   .forEach(addressType -> {
+                       final List<Address> addresses = notification.getRecipients(addressType)
+                                                                   .flatMap(this::parseAddress)
+                                                                   .collect(toList());
+                       try {
+                           message.setRecipients(mapType(addressType), addresses.toArray(new Address[addresses.size()]));
+                       } catch (final MessagingException exception) {
+                           throw new NotificationException(exception);
+                       }
+                   });
+
+            final Subject subject = notification.getSubject();
+            message.setSubject(subject.toString(), subject.getCharsetName());
+
+            notification.apply(new MimePartCreator(message));
+
+            message.saveChanges();
+
+            final Transport transport = session.getTransport();
+            Arrays.stream(listeners)
+                  .forEachOrdered(transport::addTransportListener);
+            try {
+                transport.connect();
+                transport.sendMessage(message, message.getAllRecipients());
+            } finally {
+                transport.close();
+            }
+
+        } catch (final MessagingException exception) {
+            throw new NotificationException(exception);
+        }
+    }
+
+    private Stream<Address> parseAddress(final String recipient) {
+        try {
+            return Arrays.stream(InternetAddress.parse(recipient));
+        } catch (final AddressException exception) {
+            throw new IllegalArgumentException(exception);
+        }
+    }
+
+}
--- src/main/java/pl/ais/commons/application/notification/mail/MailSender.java
+++ src/main/java/pl/ais/commons/application/notification/mail/MailSender.java
@@ -1,113 +1,25 @@
 package pl.ais.commons.application.notification.mail;
 
-import pl.ais.commons.application.notification.AddressType;
 import pl.ais.commons.application.notification.AddressedNotification;
-import pl.ais.commons.application.notification.NotificationException;
-import pl.ais.commons.application.notification.component.Subject;
 
-import javax.mail.Address;
-import javax.mail.Message.RecipientType;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.event.TransportListener;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.EnumSet;
-import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
+ * Defines the API contract for Mail Sender.
+ *
  * @author Warlock, AIS.PL
  * @since 1.2.1
  */
-public final class MailSender implements BiConsumer<AddressedNotification, TransportListener[]> {
+public interface MailSender extends BiConsumer<AddressedNotification, TransportListener[]> {
 
-    private final Session session;
-
-    public MailSender(final Session session) {
-        this.session = session;
-    }
-
+    /**
+     * Accepts given notification for the delivery.
+     *
+     * @param notification a notification to be delivered
+     * @param listeners    listeners watching the delivery process
+     */
     @Override
-    public void accept(final AddressedNotification notification, final TransportListener... listeners) {
-
-        final MimeMessage message = new MimeMessage(session);
-        try {
-            message.setSentDate(new Date());
-
-            final String sender = notification.getSender();
-            if ((null == sender) || sender.isEmpty()) {
-                message.setFrom();
-            } else {
-                final InternetAddress senderAddress = new InternetAddress(sender);
-                message.setFrom(senderAddress);
-                message.setReplyTo(new Address[] {senderAddress});
-            }
-
-            EnumSet.allOf(AddressType.class)
-                   .forEach(addressType -> {
-                       final List<Address> addresses = notification.getRecipients(addressType)
-                                                                   .flatMap(this::parseAddress)
-                                                                   .collect(Collectors.toList());
-                       try {
-                           message.setRecipients(mapType(addressType), addresses.toArray(new Address[addresses.size()]));
-                       } catch (final MessagingException exception) {
-                           throw new NotificationException(exception);
-                       }
-                   });
-
-            final Subject subject = notification.getSubject();
-            message.setSubject(subject.toString(), subject.getCharsetName());
-
-            notification.apply(new MimePartCreator(message));
-
-            message.saveChanges();
-
-            final Transport transport = session.getTransport();
-            Arrays.stream(listeners)
-                  .forEachOrdered(transport::addTransportListener);
-            try {
-                transport.connect();
-                transport.sendMessage(message, message.getAllRecipients());
-            } finally {
-                transport.close();
-            }
-
-        } catch (final MessagingException exception) {
-            throw new NotificationException(exception);
-        }
-    }
-
-    private RecipientType mapType(final AddressType type) {
-        final RecipientType result;
-        switch (type) {
-            case BCC:
-                result = RecipientType.BCC;
-                break;
-
-            case CC:
-                result = RecipientType.CC;
-                break;
-
-            default:
-                result = RecipientType.TO;
-                break;
-        }
-        return result;
-    }
-
-    private Stream<Address> parseAddress(final String recipient) {
-        try {
-            return Arrays.stream(InternetAddress.parse(recipient));
-        } catch (final AddressException exception) {
-            throw new IllegalArgumentException(exception);
-        }
-    }
+    void accept(AddressedNotification notification, TransportListener... listeners);
 
 }
